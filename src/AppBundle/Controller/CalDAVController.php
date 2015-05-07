9adb659289f162ba1d1ab560405a4673bdb018e8
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-05-07 10:46:54 +0200

    Implements ElasticSearch Backend (WIP)

--- src/AppBundle/Controller/CalDAVController.php
+++ src/AppBundle/Controller/CalDAVController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use PDO;
 use Sabre;
+use Elasticsearch;
+use AppBundle\Backend\ES;
 
 class CalDAVController extends Controller
 {
@@ -20,6 +22,8 @@ class CalDAVController extends Controller
 
         $pdo = new PDO('mysql:dbname=sabredav;host=127.0.0.1','root','');
 
+        $es = new Elasticsearch\Client();
+
         /*function exception_error_handler($errno, $errstr, $errfile, $errline ) {
 		    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 		}
@@ -27,7 +31,8 @@ class CalDAVController extends Controller
 
 		#Backends
 		$authBackend = new Sabre\DAV\Auth\Backend\PDO($pdo);
-		$calendarBackend = new Sabre\CalDAV\Backend\PDO($pdo);
+		//$calendarBackend = new Sabre\CalDAV\Backend\PDO($pdo);
+		$calendarBackend = new \AppBundle\Backend\ES($es,$pdo);
 		$principalBackend = new Sabre\DAVACL\PrincipalBackend\PDO($pdo);
 
 		$tree = [
@@ -61,9 +66,12 @@ class CalDAVController extends Controller
 		$server->exec();
 		$server->httpResponse->setHeader("Content-Security-Policy", "allow 'self';");
 
-		$this->logIt($request,$server->httpResponse);
+		//$this->logIt($request,$server->httpResponse);
 
-		return new Response($server->httpResponse->getBody(),$server->httpResponse->getStatus(),$server->httpResponse->getHeaders());
+		if (is_string($server->httpResponse->getBody()))
+			return new Response($server->httpResponse->getBody(),$server->httpResponse->getStatus(),$server->httpResponse->getHeaders());
+		else 
+			return new Response(stream_get_contents($server->httpResponse->getBody()),$server->httpResponse->getStatus(),$server->httpResponse->getHeaders());
     }
 
     private function logIt($request,$response) {
