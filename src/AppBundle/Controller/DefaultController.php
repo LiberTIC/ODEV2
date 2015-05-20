29f2b8d223798b527be0d0fc763ffe113bdcb52d
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-05-20 11:31:23 +0200

    Fix date bug

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,19 +26,64 @@ class DefaultController extends Controller
         );
         $client = new Elasticsearch\Client($params);
         $manager = new ESManager($client);
-        $event = $manager->simpleGet('calendarobjects',15)['_source'];
+        $event = $manager->simpleGet('calendarobjects',13)['_source'];
 
 
         $converter = $this->container->get('converter');
 
-        $data = $converter->convert('json','icalendar',$event['vobject']);
+        $data = <<<VCF
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Apple Inc.//Mac OS X 10.9.5//EN
+CALSCALE:GREGORIAN
+BEGIN:VTIMEZONE
+TZID:Europe/Paris
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+0100
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
+DTSTART:19810329T020000
+TZNAME:UTC+2
+TZOFFSETTO:+0200
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+0200
+RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
+DTSTART:19961027T030000
+TZNAME:UTC+1
+TZOFFSETTO:+0100
+END:STANDARD
+END:VTIMEZONE
+BEGIN:VEVENT
+CREATED:20150520T075724Z
+UID:0494770F-374C-4AA8-9920-83CFDCE79E0E
+DTEND;TZID=Europe/Paris:20150521T130000
+TRANSP:OPAQUE
+SUMMARY:Nouvel événement
+DTSTART;TZID=Europe/Paris:20150521T120000
+DTSTAMP:20150520T075809Z
+SEQUENCE:2
+URL;VALUE=URI:projet-ode.fr/event/170
+END:VEVENT
+END:VCALENDAR
+
+VCF;
+
+        
+
+        $data = $converter->convert('icalendar','json',$data);
+
+        $event['vobject'] = $data;
+        $manager->simpleIndex('calendarobjects', 13, $event);
+
+
+        //$data = $converter->convert('icalendar','json',$event['calendardata']);
 
         //$data = $converter->convert('icalendar','json',$data);
-        $response = new Response($data->serialize());
+        //$response = new Response($data->serialize());
 
-        //$response = new Response(json_encode($data, JSON_PRETTY_PRINT));
+        $response = new Response(json_encode($data, JSON_PRETTY_PRINT));
         //$response = new Response(implode(',',array_keys($event)));
-        //$response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Content-Type', 'application/json');
 
         return $response;
     }
