cac4496f21e1ef315b20d75e9aec749172511f11
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-06-04 18:18:53 +0200

    A good DateTimePicker is important

--- src/AppBundle/Form/Type/EventType.php
+++ src/AppBundle/Form/Type/EventType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EventType extends AbstractType
 {
@@ -14,7 +15,7 @@ class EventType extends AbstractType
             ->add('nom','text', array(
                 'label' => "Titre (*)",
                 'attr' => array(
-                    'placeholder' => "Some text",
+                    'placeholder' => "Titre",
                 )
             ))
             ->add('description','textarea',array(
@@ -23,10 +24,32 @@ class EventType extends AbstractType
             )
             ->add('categorie','text')
             ->add('tags','text')
-            ->add('date_debut','genemu_jquerydate', array('widget' => 'single_text'))
-            ->add('date_fin','genemu_jquerydate', array('widget' => 'single_text'))
-            ->add('lieu','text')
-            ->add('emplacement','text');
+            ->add('date_debut', 'datetime',array(
+                'widget' => 'single_text',
+                'horizontal_input_wrapper_class' => 'col-lg-5',
+                'datetimepicker' => true,
+                'attr' => array('placeholder' => '4 Juin 2015 - 20:30')
+            ))
+            ->add('date_fin', 'datetime',array(
+                'widget' => 'single_text',
+                'horizontal_input_wrapper_class' => 'col-lg-5',
+                'datetimepicker' => true,
+                'attr' => array('placeholder' => '4 Juin 2015 - 23:30')
+            ))
+            ->add('lieu','text', array(
+                'label' => "Lieu",
+                'attr' => array(
+                    'placeholder' => "Stéréolux, Nantes, France"
+                    )
+                )
+            )
+            ->add('emplacement','text', array(
+                'label' => "Emplacement",
+                'attr' => array(
+                    'placeholder' => "Salle de concert n°3"
+                    )
+                )
+            );
     }
 
     public function getName()
