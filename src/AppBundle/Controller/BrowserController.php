dce5469fa5d6862c6f7f65cad0a7dfc68921da49
Thibaud Courtoison <do.not.press.enter@gmail.com>
2015-06-04 11:19:11 +0200

    Add Controller + routes

---
+++ src/AppBundle/Controller/BrowserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class BrowserController extends Controller
+{
+
+    public function eventCreateAction() {
+
+        return new Response("eventCreateAction");
+    }
+
+    public function eventReadAction($uid) {
+
+        return new Response("eventReadAction / uid: ".$uid);
+    }
+
+    public function eventUpdateAction($uid) {
+
+        return new Response("eventUpdateAction / uid: ".$uid);
+    }
+
+    public function eventDeleteAction($uid) {
+
+        return new Response("eventDeleteAction / uid: ".$uid);
+    }
+
+}
